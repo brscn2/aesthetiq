659b0522cda415d1d8ff5e3574f3111aaf22b9ff
Atakan Sucu <103220855+atakansucu@users.noreply.github.com>
2026-02-04 18:23:10 +0100

    Update MongoDB URI to use 'localhost'

--- backend/src/config/database.config.ts
+++ backend/src/config/database.config.ts
@@ -1,6 +1,6 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('database', () => ({
-  uri: process.env.MONGODB_URI || process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/aesthetiq',
+  uri: process.env.MONGODB_URI || process.env.MONGO_URI || 'mongodb://localhost/aesthetiq',
 }));
 
